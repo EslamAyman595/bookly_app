0a34f020b3bfc2b085e122e8c0b8bccf82a90faf
Eslam Ayman <eslamaimn595@gmail.com>
2024-02-15 18:47:15 +0200

    adding sliding animation to splash view

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,22 +1,48 @@
+import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
-const SplashViewBody({ Key? key }) : super(key: key);
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
 
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slideAnimation = Tween<Offset>(begin: Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+    // animationController.reverse();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
-  
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,//ممكن تاخدوwidth اكبر children    تاخد
+      crossAxisAlignment:
+          CrossAxisAlignment.stretch, //ممكن تاخدوwidth اكبر children    تاخد
 
       children: [
         Image.asset(AssetsData.logo),
         SizedBox(
           height: 5,
         ),
-      const   Text('Read Free Books ' ,textAlign: TextAlign.center,style: TextStyle(fontSize:20 ),)
+        SlidingText(slideAnimation: slideAnimation)
       ],
     );
   }
