caf802cf35e34fca009b0a6d4f907f930074fff7
Eslam Ayman <eslamaimn595@gmail.com>
2024-02-18 18:23:54 +0200

    create api service

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -2,7 +2,7 @@ import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart'
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-Future<Either<Faliure,List<BookModel>>>  fetchFeaturesBooks();
-Future<Either<Faliure,List<BookModel>>>  fetchBestSellerBooks();
+abstract class HomeRepo {
+  Future<Either<Faliure, List<BookModel>>> fetchFeaturesBooks();
+  Future<Either<Faliure, List<BookModel>>> fetchBestSellerBooks();
 }
