67bb66076a461568b05590489cbc7e502cdf3af4
Eslam Ayman <eslamaimn595@gmail.com>
2024-02-18 23:34:57 +0200

    create featured book cubit state

--- lib/Features/home/data/repos/home_reop_impl.dart
+++ lib/Features/home/data/repos/home_reop_impl.dart
@@ -34,9 +34,29 @@ class HomeRepoImpl implements HomeRepo{
     }
   }
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturesBooks() {
-    // TODO: implement fetchFeaturesBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturesBooks() async{
+     try {
+  var data = await apiService.get(endPoints: 'volumes?Filtering=free-ebooks&q=subject:Programmingt');
+
+  List<BookModel>books=[];
+  for(var item in data["items"]){
+    books.add(BookModel.fromJson(item));
+    
+  }
+  return right(books);
+}catch (e) {
+      // ignore: deprecated_member_use
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 
 }
