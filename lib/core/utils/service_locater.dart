f1dcbeb48e47e7035a2b21b2815754e240c3c5b9
Eslam Ayman <eslamaimn595@gmail.com>
2024-02-22 15:01:33 +0200

    setup service locater

---
+++ lib/core/utils/service_locater.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/Features/home/data/repos/home_reop_impl.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocater() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      getIt.get<ApiService>()
+      // ApiService(
+      //   Dio(),
+      // ),
+    ),
+  );
+}
